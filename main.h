575cd81cfe376292316ddc8c8277d45a2b4a4428
PhilipsTechniq <philipdata73@gmail.com>
2023-08-21 11:53:57 -0700

    if this can create mental illness

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
@@ -115,5 +114,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
 
